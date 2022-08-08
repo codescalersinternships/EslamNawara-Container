347b10a37daaa2686bf629571a0f22cc4d13bfdd
Eslam-Nawara <eslamnawara44@gmailcom>
2022-08-08 12:58:11 +0200

    Add busybox rootfs

--- container.go
+++ container.go
@@ -5,12 +5,15 @@ import (
 	"os"
 	"os/exec"
 	"path"
-	"path/filepath"
 	"strconv"
 	"syscall"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		panic("Few arguments")
+	}
+
 	switch os.Args[1] {
 	case "run":
 		run()
@@ -27,37 +30,46 @@ func run() {
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Credential: &syscall.Credential{
+			Uid: uint32(syscall.Getuid()),
+			Gid: uint32(syscall.Getgid()),
+		},
+		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER,
 		Unshareflags: syscall.CLONE_NEWNS,
+		UidMappings:  []syscall.SysProcIDMap{{ContainerID: 0, HostID: syscall.Getuid(), Size: 1}},
+		GidMappings:  []syscall.SysProcIDMap{{ContainerID: 0, HostID: syscall.Getgid(), Size: 1}},
 	}
-	cmd.Run()
+
+	check(cmd.Run())
 }
+
 func child() {
 	fmt.Println("Running as", os.Getpid())
 
-	syscall.Sethostname([]byte("container"))
-	syscall.Chroot("./alpine-fs")
-	syscall.Chdir("/")
-	syscall.Mount("proc", "proc", "proc", 0, "")
+	cgroup()
+
+	check(syscall.Sethostname([]byte(NameGenerator())))
+	check(syscall.Chroot("./rootfs"))
+	check(syscall.Chdir("/"))
+	check(syscall.Mount("proc", "proc", "proc", 0, "rw"))
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	cmd.Run()
-	syscall.Unmount("/proc", 0)
+
+	check(cmd.Run())
+	check(syscall.Unmount("/proc", 0))
 }
 
 func cgroup() {
-	cgroups := "/sys/fs/cgroup/containers"
-	pids := filepath.Join(cgroups, "pids")
-	err := os.Mkdir(filepath.Join(pids, "eslam"), 0777)
-	if err != nil && os.IsExist(err) {
-		panic(err)
-	}
-	check(os.WriteFile(path.Join(pids, "pids.max"), []byte("30"), 0700))
-	check(os.WriteFile(path.Join(pids, "notify_on_release"), []byte("1"), 0700))
+	pids := "/sys/fs/cgroup/pids"
+	os.MkdirAll(pids, 0755)
+
+	check(os.WriteFile(path.Join(pids, "pids.max"), []byte("10"), 0700))
 	check(os.WriteFile(path.Join(pids, "cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
 }
 
